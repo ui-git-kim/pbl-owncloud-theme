6f3adc8b8ad0e5f2f2761e2b39d3ae345d61b7c1
Kim <kim@uniqueicon.com.au>
2025-07-20 16:20:58 +1000

    php defaults updated

--- defaults.php
+++ defaults.php
@@ -1,23 +1,4 @@
 <?php
-/**
- * @author Björn Schießle <schiessle@owncloud.com>
- * @author Jan-Christoph Borchardt, http://jancborchardt.net
- * @copyright Copyright (c) 2018, ownCloud GmbH
- * @license AGPL-3.0
- *
- * This code is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License, version 3,
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
-
 class OC_Theme {
 
 	/**
@@ -25,47 +6,7 @@ class OC_Theme {
 	 * @return string URL
 	 */
 	public function getBaseUrl() {
-		return 'https://owncloud.org';
-	}
-
-	/**
-	 * Returns the URL where the sync clients are listed
-	 * @return string URL
-	 */
-	public function getSyncClientUrl() {
-		return 'https://owncloud.org/install';
-	}
-
-	/**
-	 * Returns the URL to the App Store for the iOS Client
-	 * @return string URL
-	 */
-	public function getiOSClientUrl() {
-		return 'https://apps.apple.com/us/app/id1359583808';
-	}
-
-	/**
-	 * Returns the AppId for the App Store for the iOS Client
-	 * @return string AppId
-	 */
-	public function getiTunesAppId() {
-		return '1359583808';
-	}
-
-	/**
-	 * Returns the URL to Google Play for the Android Client
-	 * @return string URL
-	 */
-	public function getAndroidClientUrl() {
-		return 'https://play.google.com/store/apps/details?id=com.owncloud.android';
-	}
-
-	/**
-	 * Returns the documentation URL
-	 * @return string URL
-	 */
-	public function getDocBaseUrl() {
-		return 'https://doc.owncloud.com';
+		return 'https://cloud.pbl.events/';
 	}
 
 	/**
@@ -73,7 +14,7 @@ class OC_Theme {
 	 * @return string title
 	 */
 	public function getTitle() {
-		return 'Custom Cloud - Custom Claim';
+		return 'The Photo Booth-less Event Galleries';
 	}
 
 	/**
@@ -81,15 +22,7 @@ class OC_Theme {
 	 * @return string title
 	 */
 	public function getName() {
-		return 'Custom Cloud';
-	}
-
-	/**
-	 * Returns the short name of the software containing HTML strings
-	 * @return string title
-	 */
-	public function getHTMLName() {
-		return '<b>Custom</b> Cloud';
+		return 'PBL Event Galleries';
 	}
 
 	/**
@@ -97,7 +30,7 @@ class OC_Theme {
 	 * @return string entity name
 	 */
 	public function getEntity() {
-		return 'Custom Cloud Co.';
+		return 'The Photo Booth-less';
 	}
 
 	/**
@@ -105,52 +38,16 @@ class OC_Theme {
 	 * @return string slogan
 	 */
 	public function getSlogan() {
-		return 'Your custom cloud, personalized for you!';
+		return '<span style="color: #ffffff;">Picture Perfect Fun</span><span style="color: #9C569D;">.</span>';
 	}
 
-	/**
-	 * Returns logo claim
-	 * @return string logo claim
-	 */
-	public function getLogoClaim() {
-		return '<a href="https://example.com">Custom Claim</a>';
-	}
-
-	public function getPrivacyPolicyUrl() {
-		try {
-			return \OC::$server->getConfig()->getAppValue('core', 'legal.privacy_policy_url', '');
-		} catch (\Exception $e) {
-			return '';
-		}
-	}
-
-	public function getImprintUrl() {
-		try {
-			return \OC::$server->getConfig()->getAppValue('core', 'legal.imprint_url', '');
-		} catch (\Exception $e) {
-			return '';
-		}
-	}
-
-	public function getL10n() {
-		return \OC::$server->getL10N('core');
-	}
-
-	/**
+		/**
 	 * Returns short version of the footer
 	 * @return string short footer
 	 */
 	public function getShortFooter() {
-		$l10n = $this->getL10n();
-		$footer = '© '.date("Y").' <a href="'.$this->getBaseUrl().'" target="_blank\">'.$this->getEntity().'</a>'.
-			'<br/>' . $this->getSlogan();
-		if ($this->getImprintUrl() !== '') {
-			$footer .= '<span class="nowrap"> | <a href="' . $this->getImprintUrl() . '" target="_blank">' . $l10n->t('Imprint') . '</a></span>';
-		}
-
-		if ($this->getPrivacyPolicyUrl() !== '') {
-			$footer .= '<span class="nowrap"> | <a href="'. $this->getPrivacyPolicyUrl() .'" target="_blank">'. $l10n->t('Privacy Policy')	 .'</a></span>';
-		}
+		$footer = '<a href="https://www.photoboothless.com.au" target="_blank">'.$this->getEntity().'</a>'.
+			' - ' . $this->getSlogan();
 		return $footer;
 	}
 
@@ -159,30 +56,17 @@ class OC_Theme {
 	 * @return string long footer
 	 */
 	public function getLongFooter() {
-		$l10n = $this->getL10n();
-		$footer = '© '.date("Y").' <a href="'.$this->getBaseUrl().'" target="_blank\">'.$this->getEntity().'</a>'.
-			'<br/>' . $this->getSlogan();
-		if ($this->getImprintUrl() !== '') {
-			$footer .= '<span class="nowrap"> | <a href="' . $this->getImprintUrl() . '" target="_blank">' . $l10n->t('Imprint') . '</a></span>';
-		}
-
-		if ($this->getPrivacyPolicyUrl() !== '') {
-			$footer .= '<span class="nowrap"> | <a href="'. $this->getPrivacyPolicyUrl() .'" target="_blank">'. $l10n->t('Privacy Policy') .'</a></span>';
-		}
+		$footer = '<a href="https://www.photoboothless.com.au" target="_blank">'.$this->getEntity().'</a>'.
+			' - ' . $this->getSlogan();
 		return $footer;
 	}
-
-	public function buildDocLinkToKey($key) {
-		return $this->getDocBaseUrl() . '/server/10.0/go.php?to=' . $key;
-	}
-
 
 	/**
 	 * Returns mail header color
 	 * @return string
 	 */
 	public function getMailHeaderColor() {
-		return '#745bca';
+		return '#9C569D';
 	}
 
 }
